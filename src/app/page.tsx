638f9118f425fb87ec971a0090df44bde35c18f4
雨凌 <yulinglsy@163.com>
2025-03-23 16:04:10 +0800

    项目初始化

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button"
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen" >
+      <Button>Click me</Button>
+    </div>
+  )
+}
